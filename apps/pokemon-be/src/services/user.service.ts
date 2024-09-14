54aff2a1498b608e1c885f41a30ab3fc18d96217
leephan2k1 <pttl2k1@gmail.com>
2024-09-14 18:03:21 +0700

    be - toggle favorite pokemon

--- apps/pokemon-be/src/services/user.service.ts
+++ apps/pokemon-be/src/services/user.service.ts
@@ -1,7 +1,11 @@
 import { Inject, Injectable } from '@nestjs/common';
+import { IPokemonRepository } from 'src/contracts/repository-contracts/pokemon-repository.interface';
 import { IUserRepository } from 'src/contracts/repository-contracts/user-repository.interface';
 import { IUserService } from 'src/contracts/service-contracts/user-service.interface';
+import { FavoritePokemonRequest } from 'src/controllers/user/favorite-pokemon.request';
+import { FavoritePokemonResponse } from 'src/controllers/user/favorite-pokemon.response';
 import { User } from 'src/models/user.model';
+import { either } from 'src/utils/either';
 import { FindOptionsWhere } from 'typeorm';
 
 @Injectable()
@@ -9,6 +13,9 @@ export class UserService implements IUserService {
   constructor(
     @Inject(IUserRepository)
     private readonly usersRepository: IUserRepository,
+
+    @Inject(IPokemonRepository)
+    private readonly pokemonRepository: IPokemonRepository,
   ) {}
 
   public async getUserByConditions(user: Partial<User>) {
@@ -37,5 +44,41 @@ export class UserService implements IUserService {
     const { password, ...newUserWithoutPassword } = newUser;
 
     return newUserWithoutPassword as Omit<User, 'password'>;
+  }
+
+  public async toggleFavoritePokemon(
+    reqBody: FavoritePokemonRequest,
+    currentUser: User,
+  ) {
+    const { isMark, pokemonId } = reqBody;
+    const users = await this.usersRepository.findWithRelations({
+      where: { id: currentUser.id },
+      relations: {
+        favoritePokemons: true,
+      },
+    });
+    const user = users[0];
+
+    if (isMark) {
+      const existingPokemon =
+        await this.pokemonRepository.findOneById(+pokemonId);
+
+      if (
+        existingPokemon &&
+        !user.favoritePokemons.some((fp) => fp.id === pokemonId)
+      ) {
+        user.favoritePokemons.push(existingPokemon);
+      } else {
+        return either.error(new FavoritePokemonResponse({ status: 'failed' }));
+      }
+    } else {
+      user.favoritePokemons = user.favoritePokemons.filter(
+        (fp) => fp.id !== pokemonId,
+      );
+    }
+
+    await this.usersRepository.save(user);
+
+    return either.of(new FavoritePokemonResponse({ status: 'success' }));
   }
 }
