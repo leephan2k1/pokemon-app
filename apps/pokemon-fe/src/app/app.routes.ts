6b437128c01514a44a23e603d99887f860ee1d67
leephan2k1 <pttl2k1@gmail.com>
2024-09-14 19:55:55 +0700

    fe - design layout & setup tailwindcss

--- apps/pokemon-fe/src/app/app.routes.ts
+++ apps/pokemon-fe/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './components/layouts/main-layout/layout.component';
+import { HomePageComponent } from './views/home-page/home-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      { path: '', component: HomePageComponent },
+      //   { path: 'about', component: BrowseComponent }
+    ],
+  },
+];
