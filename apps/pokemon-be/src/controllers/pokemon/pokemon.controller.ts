82f56590cb8e59827da11826a1ba6805f6934774
leephan2k1 <pttl2k1@gmail.com>
2024-09-14 17:19:04 +0700

    be - handle fetching pokemon list

--- apps/pokemon-be/src/controllers/pokemon/pokemon.controller.ts
+++ apps/pokemon-be/src/controllers/pokemon/pokemon.controller.ts
@@ -2,9 +2,12 @@ import { Mapper } from '@automapper/core';
 import { InjectMapper } from '@automapper/nestjs';
 import {
   Controller,
+  Get,
   HttpStatus,
   Inject,
+  Param,
   Post,
+  Query,
   Res,
   UploadedFile,
   UseGuards,
@@ -12,8 +15,11 @@ import {
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { Response } from 'express';
+import { PokemonDto } from 'src/common/dtos/pokemon.dto';
 import { JwtAuthGuard } from 'src/common/guards/jwt-auth.guard';
 import { IPokemonService } from 'src/contracts/service-contracts/pokemon-service.interface';
+import { Pokemon } from 'src/models/pokemon.model';
+import { GetPokemonQueries } from './get-pokemon-list.request';
 
 @Controller('pokemons')
 export class PokemonController {
@@ -39,5 +45,45 @@ export class PokemonController {
     const responseUpload = await this.pokemonService.importPokemonList(file);
 
     return res.status(HttpStatus.CREATED).send(responseUpload);
+  }
+
+  @Get(':id')
+  @UseGuards(JwtAuthGuard)
+  public async getPokemonDetails(
+    @Param('id') id: string,
+    @Res() res: Response,
+  ) {
+    if (isNaN(+id)) {
+      return res
+        .status(HttpStatus.BAD_REQUEST)
+        .send({ error: 'id must be a number' });
+    }
+
+    const result = await this.pokemonService.getPokemonDetails(+id);
+
+    if (result.isError()) {
+      return res.status(HttpStatus.NOT_FOUND).send(result.value);
+    }
+
+    const pokemonDto = this.mapper.map(result.value, Pokemon, PokemonDto);
+    return res.status(HttpStatus.OK).send(pokemonDto);
+  }
+
+  @Get()
+  @UseGuards(JwtAuthGuard)
+  public async getPokemonListOnDemand(
+    @Res() res: Response,
+    @Query() queries: GetPokemonQueries,
+  ) {
+    const pokemonListRes =
+      await this.pokemonService.getPokemonListOnDemand(queries);
+
+    pokemonListRes.data = this.mapper.mapArray(
+      pokemonListRes.data,
+      Pokemon,
+      PokemonDto,
+    );
+
+    return res.status(HttpStatus.OK).send(pokemonListRes);
   }
 }
--- apps/pokemon-be/src/services/pokemon.service.ts
+++ apps/pokemon-be/src/services/pokemon.service.ts
@@ -1,10 +1,16 @@
 import { Inject, Injectable } from '@nestjs/common';
 import * as csv from 'csv-parser';
 import { createReadStream, unlinkSync } from 'fs';
+import { PageInfo } from 'src/common/dtos/pagination-result.dto';
 import { IPokemonRepository } from 'src/contracts/repository-contracts/pokemon-repository.interface';
 import { IPokemonService } from 'src/contracts/service-contracts/pokemon-service.interface';
+import { GetPokemonQueries } from 'src/controllers/pokemon/get-pokemon-list.request';
+import { PokemonListResponse } from 'src/controllers/pokemon/get-pokemon-list.response';
+import { NotFoundPokemonError } from 'src/controllers/pokemon/not-found-pokemon.error';
 import { ImportPokemonResponse } from 'src/controllers/pokemon/total-uploaded.response';
 import { Pokemon } from 'src/models/pokemon.model';
+import { either } from 'src/utils/either';
+import { Between, ILike, Like } from 'typeorm';
 
 @Injectable()
 export class PokemonService implements IPokemonService {
@@ -67,5 +73,54 @@ export class PokemonService implements IPokemonService {
     });
 
     return new ImportPokemonResponse(totalSaved.length);
+  }
+
+  public async getPokemonDetails(id: number) {
+    const pokemon = await this.pokemonRepository.findOneById(id);
+
+    if (!pokemon) {
+      return either.error(new NotFoundPokemonError({ id: id }));
+    }
+
+    return either.of(pokemon);
+  }
+
+  public async getPokemonListOnDemand(queries: GetPokemonQueries) {
+    let { isLegendary, limit, maxSpeed, minSpeed, name, page, type } = queries;
+    const _limit = +limit;
+    const _page = +page;
+    minSpeed = +minSpeed;
+    maxSpeed = +maxSpeed;
+
+    const [data, count] = await this.pokemonRepository.findAndCount({
+      where: {
+        isLegendary,
+        speed: minSpeed && maxSpeed ? Between(minSpeed, maxSpeed) : undefined,
+        name: name ? ILike(`%${name}%`) : undefined,
+        type1: type,
+        type2: type,
+      },
+      take: limit ? _limit : 20,
+      skip: page ? (_page - 1) * _limit : 0,
+      order:
+        minSpeed && maxSpeed
+          ? {
+              speed: 'ASC',
+            }
+          : undefined,
+      cache: true,
+    });
+
+    // calculate page info
+    const lastPage = Math.ceil(count / _limit);
+    const pageInfo = new PageInfo({
+      total: count,
+      currentPage: _page,
+      hasNextPage: _page < _limit,
+      lastPage,
+      perPage: _limit,
+    });
+
+    return new PokemonListResponse(data, pageInfo);
   }
 }
